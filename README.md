9a2c7d4def3623217e746b5f0430e8b97c584be6
caem2 <jeongchaemi1@gmail.com>
2024-06-28 12:52:41 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 git config --global user.email "jeongchaemi1@gmail.com"
 git config --global user.name "caem2"
+
+추가추가# gitmas
---
+++ java_work/ex01/src/ex01/ex03.java
@@ -0,0 +1,23 @@
+package ex01;
+/*
+ * 여러 줄을 주석 처리
+ * ctrl+z 뒤로 가기
+ * ctrl+y 앞으로 가기
+ * ctrl+w 창닫는 단축키
+ */
+
+public class ex03 {
+	public static void main(String[] args) {
+		
+		System.out.println("10+3 = " + (10+3));
+		System.out.println("50/3 = " + 50 / 3);
+		System.out.println("50% = " + 50 % 3);
+		System.out.println("3*4 = " + 3 * 4);
+		System.out.println("10-7 = " + (10-7));
+		
+		// ctrl + / 주석처리 및 해제
+		//+ - 는 () 쓰기
+		
+	}
+
+}
