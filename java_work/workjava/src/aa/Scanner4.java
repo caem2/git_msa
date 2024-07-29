de58f4cba6d36554b37bec3b51fb96215c754fd1
caem2 <jeongchaemi1@gmail.com>
2024-07-29 18:03:13 +0900

    2024-07-29

--- java_work/workjava/src/aa/Scanner4.java
+++ java_work/workjava/src/aa/Scanner4.java
@@ -3,6 +3,10 @@ package aa;
 import java.util.Scanner;
 
 public class Scanner4 {
+    /*
+    문제 - 구구단 출력
+사용자로부터 하나의 정수 n 을 입력받고, 입력받은 정수 n 의 구구단을 출력하는 프로그램을 작성하세요
+     */
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
--- java_work/workjava/src/aa/Scanner6.java
+++ java_work/workjava/src/aa/Scanner6.java
@@ -3,6 +3,21 @@ package aa;
 import java.util.Scanner;
 
 public class Scanner6 {
+    /*
+    문제 - 이름과 나이 반복
+사용자로부터 이름과 나이를 반복해서 입력받고, 입력받은 이름과 나이를 출력하는 프로그램을 작성하세요. 사용
+자가 "종료"를 입력하면 프로그램이 종료되어야 합니다.
+다음 실행 결과 예시를 참고해주세요.
+실행 결과 예시 ```
+이름을 입력하세요 (종료를 입력하면 종료): 자바
+나이를 입력하세요: 30
+입력한 이름: 자바, 나이: 30
+이름을 입력하세요 (종료를 입력하면 종료): 하니
+나이를 입력하세요: 20
+입력한 이름: 하니, 나이: 20
+이름을 입력하세요 (종료를 입력하면 종료): 종료
+프로그램을 종료합니다.
+     */
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -17,6 +32,7 @@ public class Scanner6 {
             }
             System.out.print("나이를 입력하셈:");
             int age = input.nextInt();
+            input.nextInt();//숫자 입력 후의 줄바꿈 처리
 
             System.out.println("입력한이름: " + name + ", 나이:" + age);
         }
